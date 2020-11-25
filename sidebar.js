47809fcff3109250722139b5a6eb2680ca027fc6
Masterx-31 <koberfilip2@gmail.com>
2020-11-25 11:40:04 +0100

    wersja 1.0.0 strony

--- sidebar.js
+++ sidebar.js
@@ -3,16 +3,18 @@ var sidebar = document.getElementById("sidebar");
 var main = document.getElementById("main");
 var content = document.getElementById("content");
 
+document.getElementById("sidebar").style.width = "4%"
+
 sidebarBtn.onclick = function() {
-    if(document.getElementById("sidebar").style.width == "3%") {
+    if(document.getElementById("sidebar").style.width == "4%") {
         content.style.visibility = "visible";
         sidebar.style.width = "250px";
         main.style.marginLeft = "250px";
         sidebarBtn.textContent = "<"
     }else {
         content.style.visibility = "hidden";
-        sidebar.style.width = "3%";
-        main.style.marginLeft = "3%";  
+        sidebar.style.width = "4%";
+        main.style.marginLeft = "4%";  
         sidebarBtn.textContent = ">"
     };
  
